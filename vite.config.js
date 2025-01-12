8daac9913facce3e3dc57ab848a333bc27bac825
Haziq <haziqemir6@gmail.com>
2025-01-12 22:25:17 +0800

    fix 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build', // Ensure this matches your structure
+        manifest: true,
+        emptyOutDir: true,
+        rollupOptions: {
+          input: './resources/js/app.js', // Adjust this to your entry point
+        },
+      },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
