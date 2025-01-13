5c6899476b73e506311464555fde5520d83bb119
Azreen <nr.azreenasari@gmail.com>
2025-01-13 13:02:41 +0800

    change title

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mixue - Home</title>
+    <title>Mixue</title>
     <!-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> Add your custom CSS here -->
     <style>
         body {
